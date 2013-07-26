f3af244303a00f5ecfbcd9aa562cedfaa836f774
Josh Shemas <jshemas@hotmail.com>
2013-07-26 19:05:27 -0400

    clean up app.js and set dbpath to localhost

--- app/app.js
+++ app/app.js
@@ -1,52 +1,44 @@
+var express = require('express'),
+	routes = require('./routes'),
+	User = require('./routes/user'),
+	Game = require('./routes/game'),
+	http = require('http'),
+	expressLayouts = require('express-ejs-layouts'),
+	path = require('path'),
+	app = express(),
+	mongoose = require('mongoose');
 
-/**
- * Module dependencies.
- */
+//shared server
+//var dbPath = 'mongodb://192.168.241.233/pingpong';
+//local server
+var dbPath = 'mongodb://localhost/local';
 
-var express = require('express')
-  , routes = require('./routes')
-  , User = require('./routes/user')
-  , Game = require('./routes/game')
-  , http = require('http')
-  , expressLayouts = require('express-ejs-layouts')
-  , path = require('path');
-
-
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://192.168.241.233/pingpong')
-
-var app = express();
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs')
-app.set('layout', 'layout');
-app.use(expressLayouts);
-
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
-app.use(require('stylus').middleware(__dirname + '/public'));
-app.use(express.static(path.join(__dirname, 'public')));
+// configure Express
+app.configure(function() {
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+	app.set('layout', 'layout');
+	app.use(expressLayouts);
+	app.use(express.favicon());
+	//app.use(express.logger('dev')); //this spams my logs
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.cookieParser('your secret here'));
+	app.use(express.session());
+	app.use(app.router);
+	app.use(require('stylus').middleware(__dirname + '/public'));
+	app.use(express.static(path.join(__dirname, 'public')));
+	mongoose.connect(dbPath, function onMongooseError(err) {
+		if (err) throw err;
+	});
+});
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback () {
-	console.log("DB Online!");
-});
 
 var playerSchema = mongoose.Schema({
 	fname: String,
@@ -78,8 +70,6 @@ var Teams = mongoose.model('teams', teamSchema);
 
 user = new User({Players: Players});
 game = new Game({Games: Games, Players: Players});
-
-
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
