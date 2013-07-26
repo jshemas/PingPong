3819d49871081bfa074847ab6be58ff58c609e08
Corey Snyder <cornhead15@hotmail.com>
2013-07-26 17:23:25 -0400

    Updated game page to show player details

--- myapp/routes/game.js
+++ myapp/routes/game.js
@@ -1,3 +1,5 @@
+var async = require('async');
+
 
 /*
  * GET Games listing.
@@ -5,7 +7,6 @@
 
 function Game(config){
 	this.config = config;
-	console.log(this.config);
 
 }
 
@@ -15,39 +16,101 @@ module.exports = Game;
 Game.prototype.list = function(req, res){
 
 	var that = this;
+	var games = [];
+	var players = [];
 
 	async.parallel([
-		function(){
+		function(pcb){ // Get All Available Players
 			that.config.Players.find(function (err, players) {
 				if (err){ // TODO handle err
 					console.log(players)
 				} else{
-					res.render('players', { title: 'Players', players: players });
+					pcb(null,players)
 				}
 
 			});
 		},
-		function(){
-			that.config.Games.find(function (err, players) {
+		function(pcb){ // Get all Games
+			that.config.Games.find(function (err, games) {
 				if (err){ // TODO handle err
-					console.log(players)
+					console.log(games)
 				} else{
+					console.log("games Found", games);
+					pcb(null,games)
 
-					res.render('games', { title: 'Games Played', games: [] });
 				}
 
 			});
 		}
-	], callback);
+	], function(error, args){
+
+		var myPlayers = args[0];
+		var myGames = args[1];
+
+		myGames.forEach(function(game, i){
+			console.log("GAME", i);
+			myPlayers.forEach(function(player, j){
+				console.log("PLAYER", player);
+
+				if(player["_id"] == game.redPlayer){
+					console.log("Matched to RedPlayer", player);
+					game.redPlayerDetails = player;
+				}
+				if(player["_id"] == game.bluePlayer){
+					console.log("Matched to Blue Player", player);
+					game.bluePlayerDetails = player;
+				}
+			});
+		});
+
+		console.log("MY Players", myPlayers);
+		console.log("My Games", myGames);
+
+		res.render('games', { title: 'Games Played', games: myGames, players: myPlayers });
+	});
 
 };
 
 Game.prototype.add = function(req, res){
-	this.config.Games.find(function (err, players) {
-		if (err){ // TODO handle err
-			console.log(players)
-		} else{
-			//res.render('games', { title: 'Players', players: players });
+	console.log('req', req.body);
+//	console.log('req', res);
+
+	var redPlayer = req.body.redPlayer;
+	var bluePlayer = req.body.bluePlayer;
+
+	var matches = [
+		{
+			redScore: req.body["match1-redPlayer"] || 0,
+			blueScore: req.body["match1-bluePlayer"] || 0
+		},
+		{
+			redScore: req.body["match2-redPlayer"] || 0,
+			blueScore: req.body["match2-bluePlayer"] || 0
+		}
+	];
+
+	if(req.body["match3-redPlayer"] || req.body["match3-bluePlayer"] ){
+		matches.push({
+			redScore: req.body["match3-redPlayer"] || 0,
+			blueScore: req.body["match3-bluePlayer"] || 0
+		});
+	}
+
+	var newGame = new this.config.Games({
+		redPlayer: redPlayer,
+		bluePlayer: bluePlayer,
+		matches: matches
+	});
+
+	newGame.save(function (err, newGame) {
+
+		if (err){ // TODO handle the error
+			console.log("Game Add Failed: ", err);
+			res.redirect(500, "/games")
+		}
+		else{
+			console.log("Game Added");
+			res.redirect("/games");
 		}
 
 	});
