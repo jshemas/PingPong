fe881879b1179022e5710aa248d61456c1f8daeb
Eric Wilson <wilson.eric.n@gmail.com>
2013-08-15 05:59:48 -0400

    Added basic ELO rating function

---
+++ elo.py
@@ -0,0 +1,14 @@
+from __future__ import division
+
+def pow_rate(rating):
+    return 10**(rating/400.0)
+
+def exp_score(r1,r2):
+    q1 = pow_rate(r1)
+    q2 = pow_rate(r2)
+    return q1 / (q1 + q2)
+
+def delta(r1,r2,result):
+    K = 32
+    return K * (result - exp_score(r1,r2))
+
