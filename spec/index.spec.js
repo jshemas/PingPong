1bce65ccc64df79c9197e43aab86f32472ead6d9
Josh Shemas <jshemas@hotmail.com>
2013-09-26 22:05:59 -0400

    refactor tests

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,5 +1,5 @@
 var request = require('../app/node_modules/supertest'),
-	app = require('../app/app');
+	app = require('../app/app'),
 	expect = require('../app/node_modules/expect.js');
 
 console.log("Starting Tests");
@@ -46,69 +46,42 @@ var gameID;
 
 describe('GET - Load Some Pages:', function (done) {
 	it('Should load the homepage', function(done) {
-		request(app)
-			.get('')
-			.end( function(err, result) {
-				expect(result.res.statusCode).to.be(200);
-				done();
-			});
+		request(app).get('/').expect(200, done);
 	});
 	it('Should load the games', function(done) {
-		request(app)
-			.get('/matches/json')
-			.end( function(err, result) {
-				expect(result.res.statusCode).to.be(200);
-				done();
-			});
+		request(app).get('/matches/json').expect(200, done);
 	});
 	it('Should load the users', function(done) {
-		request(app)
-			.get('/users/json')
-			.end( function(err, result) {
-				expect(result.res.statusCode).to.be(200);
-				done();
-			});
+		request(app).get('/users/json').expect(200, done);
 	});
 });
 
 describe('Add Player: ', function (done) {
 	it('Adds player One to the database', function(done) {
-		request(app)
-			.post('/users')
-			.send(userData)
-			.end( function(err, result) {
-				expect(result.res.statusCode).to.be(200);
-				userID1 = result.res.body.player["_id"];
-				done();
-			});
+		request(app).post('/users').send(userData).expect(200).end(function(err, res){
+			expect(res.body.success).to.be(true);
+			userID1 = res.body.player["_id"];
+			done();
+		});
 	});
 	it('make sure player one exists', function(done) {
-		request(app)
-			.get('/users/'+userID1+'/json')
-			.end( function(err, result) {
-				expect(result.res.statusCode).to.be(200);
-				expect(result.res.body._id).to.be(userID1);
-				done();
-			});
+		request(app).get('/users/'+userID1+'/json').expect(200).end(function(err, res){
+			expect(res.body._id).to.be(userID1);
+			done();
+		});
 	});
 	it('Adds player Two to the database', function(done) {
-		request(app)
-			.post('/users')
-			.send(userData)
-			.end( function(err, result) {
-				expect(result.res.statusCode).to.be(200);
-				userID2 = result.res.body.player["_id"];
-				done();
-			});
+		request(app).post('/users').send(userData).expect(200).end(function(err, res){
+			expect(res.body.success).to.be(true);
+			userID2 = res.body.player["_id"];
+			done();
+		});
 	});
 	it('make sure player two exists', function(done) {
-		request(app)
-			.get('/users/'+userID2+'/json')
-			.end( function(err, result) {
-				expect(result.res.statusCode).to.be(200);
-				expect(result.res.body._id).to.be(userID2);
-				done();
-			});
+		request(app).get('/users/'+userID2+'/json').expect(200).end(function(err, res){
+			expect(res.body._id).to.be(userID2);
+			done();
+		});
 	});
 });
 
@@ -117,120 +90,86 @@ describe('Add Game: ', function (done) {
 		//update the test game data with the new users we added
 		gameData.redPlayer._id = userID1;
 		gameData.bluePlayer._id = userID2;
-		request(app)
-			.post('/matches')
-			.send(gameData)
-			.end( function(err, result) {
-				expect(result.res.statusCode).to.be(200);
-				expect(result.res.body.success).to.be(true);
-				//the return obj has 3 _ids in it
-				//we need to find which one is game ID
-				if(result.res.body.match[0].delete){
-					gameID = result.res.body.match[0]._id;
-				} else if(result.res.body.match[1].delete) {
-					gameID = result.res.body.match[1]._id;
-				} else {
-					gameID = result.res.body.match[2]._id;
-				};
-				done();
-			});
+		request(app).post('/matches').send(gameData).expect(200).end(function(err, res){
+			expect(res.body.success).to.be(true);
+			//the return obj has 3 _ids in it
+			//we need to find which one is game ID
+			if(res.body.match[0].delete){
+				gameID = res.body.match[0]._id;
+			} else if(res.body.match[1].delete) {
+				gameID = res.body.match[1]._id;
+			} else {
+				gameID = res.body.match[2]._id;
+			};
+			done();
+		});
 	});
 	it('make sure the new game exists', function(done) {
-		request(app)
-			.get('/matches/'+gameID+'/json')
-			.end( function(err, result) {
-				expect(result.res.statusCode).to.be(200);
-				expect(result.res.body._id).to.be(gameID);
-				done();
-			});
+		request(app).get('/matches/'+gameID+'/json').expect(200).end(function(err, res){
+			expect(res.body._id).to.be(gameID);
+			done();
+		});
 	});
 	it('Adds a game to the database - failed same user IDS', function(done) {
 		//update the test game data with the new users we added
 		gameData.redPlayer._id = userID1;
 		gameData.bluePlayer._id = userID1;
-		request(app)
-			.post('/matches')
-			.send(gameData)
-			.end( function(err, result) {
-				expect(result.res.statusCode).to.be(200);
-				expect(result.res.body.success).to.be(false);
-				done();
-			});
+		request(app).post('/matches').send(gameData).expect(200).end(function(err, res){
+			expect(res.body.success).to.be(false);
+			done();
+		});
 	});
 	it('Adds a game to the database - failed no user IDS', function(done) {
 		//update the test game data with the new users we added
 		gameData.redPlayer._id = '';
 		gameData.bluePlayer._id = '';
-		request(app)
-			.post('/matches')
-			.send(gameData)
-			.end( function(err, result) {
-				expect(result.res.statusCode).to.be(200);
-				expect(result.res.body.success).to.be(false);
-				done();
-			});
+		request(app).post('/matches').send(gameData).expect(200).end(function(err, res){
+			expect(res.body.success).to.be(false);
+			done();
+		});
 	});
 });
 
 describe("Remove Game: ", function(done){
 	it('Should mark the game as removed.', function(done){
-		request(app)
-			.get('/matches/' + gameID + '/delete')
-			.end( function(err, result) {
-				expect(result.res.statusCode).to.be(200);
-				done();
-			});
+		request(app).get('/matches/'+gameID+'/delete').expect(200).end(function(err, res){
+			//we should be setting a success flag for this call
+			done();
+		});
 	});
 	it('make sure the new game was deleted', function(done) {
-		request(app)
-			.get('/matches/'+gameID+'/json')
-			.end( function(err, result) {
-				expect(result.res.statusCode).to.be(200);
-				expect(result.res.body._id).to.be(gameID);
-				expect(result.res.body.deleted).to.be(true);
-				done();
-			});
+		request(app).get('/matches/'+gameID+'/json').expect(200).end(function(err, res){
+			expect(res.body.deleted).to.be(true);
+			expect(res.body._id).to.be(gameID);
+			done();
+		});
 	});
 });
 
 describe('Remove Player: ', function (done) {
 	it('Removes player One from database', function(done) {
-		request(app)
-			.get('/users/' + userID1 + '/delete')
-			.send()
-			.end( function(err, result) {
-				expect(result.res.statusCode).to.be(200);
-				expect(result.res.body.Success).to.be(true);
-				done();
-			});
+		request(app).get('/users/'+userID1+'/delete').expect(200).end(function(err, res){
+			expect(res.body.Success).to.be(true);
+			done();
+		});
 	});
 	it('make sure player one was deleted', function(done) {
-		request(app)
-			.get('/users/'+userID1+'/json')
-			.end( function(err, result) {
-				expect(result.res.statusCode).to.be(200);
-				expect(result.res.body.Success).to.be(false);
-				done();
-			});
+		request(app).get('/users/'+userID1+'/json').expect(200).end(function(err, res){
+			expect(res.body.Success).to.be(false);
+			done();
+		});
 	});
-	it('Removes player two from database', function(done) {
-		request(app)
-			.get('/users/' + userID2 + '/delete')
-			.send()
-			.end( function(err, result) {
-				expect(result.res.statusCode).to.be(200);
-				expect(result.res.body.Success).to.be(true);
-				done();
-			});
+	it('Removes player One from database', function(done) {
+		request(app).get('/users/'+userID2+'/delete').expect(200).end(function(err, res){
+			expect(res.body.Success).to.be(true);
+			done();
+		});
 	});
-	it('make sure player two was deleted', function(done) {
-		request(app)
-			.get('/users/'+userID2+'/json')
-			.end( function(err, result) {
-				expect(result.res.statusCode).to.be(200);
-				expect(result.res.body.Success).to.be(false);
-				done();
-			});
+	it('make sure player one was deleted', function(done) {
+		request(app).get('/users/'+userID2+'/json').expect(200).end(function(err, res){
+			expect(res.body.Success).to.be(false);
+			done();
+		});
 	});
 });
 
